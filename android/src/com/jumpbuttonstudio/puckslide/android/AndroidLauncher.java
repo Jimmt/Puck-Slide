cabcedf75bb129b3fcf2e17166aabbed31960fdd
Jimmt <quantum.physics98@gmail.com>
2015-03-09 23:22:20 -0700

    initial commit

---
+++ android/src/com/jumpbuttonstudio/puckslide/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.jumpbuttonstudio.puckslide.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.jumpbuttonstudio.puckslide.PuckSlide;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new PuckSlide(), config);
+	}
+}
---
+++ desktop/src/com/jumpbuttonstudio/puckslide/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.jumpbuttonstudio.puckslide.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.jumpbuttonstudio.puckslide.PuckSlide;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new PuckSlide(), config);
+	}
+}
